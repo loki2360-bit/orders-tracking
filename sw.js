61c46d60b270c087c8c35fb4512647bd05ba54be
loki2360-bit <loki2360@gmail.com>
2026-01-18 13:01:04 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+﻿self.addEventListener('install', e => {
+  e.waitUntil(caches.open('v1').then(cache => cache.addAll([
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json'
+  ])));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
