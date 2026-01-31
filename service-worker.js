e40e4d9a5c71afa1afe05e0d1f10a7a768b0346d
loki2360-bit <loki2360@gmail.com>
2026-01-31 21:20:20 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,2 @@
-const CACHE_NAME = 'orders-app-v2';
-const urlsToCache = [
-  '/orders-tracking/',
-  '/orders-tracking/index.html',
-  '/orders-tracking/style.css',
-  '/orders-tracking/script.js',
-  '/orders-tracking/manifest.json',
-  '/orders-tracking/icon-192.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+// Минимальный SW для PWA
+self.addEventListener('fetch', () => {});
