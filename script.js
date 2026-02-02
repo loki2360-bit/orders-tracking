b249e347f872599900c529c6e630f81ae238eed4
loki2360-bit <loki2360@gmail.com>
2026-02-02 21:55:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -184,11 +184,11 @@ function renderEarningsChart() {
 
   earningsChart = new Chart(ctx, {
     type: 'bar',
-    data: {
+     {
       labels: dates,
       datasets: [{
         label: 'Заработок, ₽',
-        data: earnings,
+         earnings,
         backgroundColor: '#ffd700',
         borderColor: '#000',
         borderWidth: 1
